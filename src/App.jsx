acea059303690f0d838adcd21c5e3b0a50e30094
marchjlim <95461451+marchjlim@users.noreply.github.com>
2025-05-21 18:46:38 +0800

    Fix bug in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/home'
-import { NotFound } from './pages/NotFound'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 import { Toaster } from '@/components/ui/toaster';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Toaster />
