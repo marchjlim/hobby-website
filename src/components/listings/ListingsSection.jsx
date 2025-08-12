1444e44c5d9df67b7a81bc86a47ccb39302ab8c1
marchjlim <95461451+marchjlim@users.noreply.github.com>
2025-08-12 21:30:50 +0800

    Update listing section

--- src/components/listings/ListingsSection.jsx
+++ src/components/listings/ListingsSection.jsx
@@ -5,28 +5,6 @@ import { useLocation } from "react-router-dom";
 import { ListingsContainer } from "./ListingsContainer";
 
 
-// const listings = [
-//     {name: "MG Rezel type C defenser a + b unit", categories: ["MG", "Regular release", "1/100", "in-stock"], url: "", image: "/listings/rezel_type_c.jpg"},
-//     {name: "MG Tallgeese III special coating", categories: ["MG", "Event-limited", "1/100", "in-stock", "special coating"], url: "", image: "/listings/mg_tallgeese_III_special_coating.png"},
-//     {name: "RG Banshee Final battle special coating", categories: ["RG", "Premium Bandai", "1/144", "in-stock", "special coating"], url: "", image: "/listings/rg_banshee_special_coating.jpg"},
-//     {name: "RG Unicorn Final battle special coating", categories: ["RG", "Gundam base limited", "1/144", "in-stock", "special coating"], url: "", image:"/listings/rg_unicorn_special_coating.jpg"},
-// ]
-
-const categories = [
-    "all",
-    "RG",
-    "MG",
-    "1/144",
-    "1/100",
-    "Regular release",
-    "Event-limited",
-    "Gundam base limited",
-    "Premium Bandai",
-    "in-stock",
-    "pre-order",
-    "Metal build",
-];
-
 export const ListingsSection = ({ refreshFlag, triggerRefresh }) => {
     const [activeTag, setActiveTag] = useState("all");
 
@@ -84,10 +62,20 @@ export const ListingsSection = ({ refreshFlag, triggerRefresh }) => {
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
             My <span className="text-primary"> Listings</span>
             </h2>
-            <p className="text-muted-foreground max-2-2xl mx-auto mb-12">
-                See a kit that you want but isn't here? Feel free to <a href="#contact">contact me </a>
+            <div className="text-muted-foreground max-2-2xl mx-auto mb-12">
+                <p>
+                See a kit that you want but isn't here? Feel free to <a href="#contact" className="text-primary">contact me </a>
                 and I will see if I can source it out for you.
-            </p>
+                </p>
+                <p>
+                    Browse through my collection of Gundam kits and accessories. 
+                    Click on the tags to filter the listings.
+                </p>
+                <p>
+                    Note: Deals made on Telegram are cheaper. See <a href="#faq" className="text-primary">FAQ</a> for more info.
+                </p>
+            </div>
+            
             
 
             <div className="flex flex-wrap justify-center gap-4 mb-12">
