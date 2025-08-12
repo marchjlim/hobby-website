06269aa7dcf1b7bda1da08fd78d95909accd0181
marchjlim <95461451+marchjlim@users.noreply.github.com>
2025-08-12 19:22:51 +0800

    Update FAQ

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -4,10 +4,11 @@ const questions_and_answers = [
     {index: 1, question: "Are your kits authentic?", answer: "Yes, everything I sell is authentic unless otherwise stated."},
     {index: 2, question: "Do you actually build gunpla?", answer: "Yep, like you, I also enjoy plamo. I'm not just a seller."},
     {index: 3, question: "Are you able to do delivery?", answer: "Currently delivery is only via lalamove or grab express, as I do not have materials for other delivery services right now."},
-    {index: 4, question: "Can you notify me on WhatsApp/other platforms when my preorder arrives?", answer: "No. There are too many buyers, and it will be hard for me to keep track of everyone's phone numbers and orders. Hence I only communicate via Carousell."},
+    {index: 4, question: "Can you notify me on WhatsApp/Telegram when my preorder arrives?", answer: "Yes. In fact, this is the preferred form of communication now especially for pre-orders. Just give me your contact."},
     {index: 5, question: "I have multiple preorders that arrive on different months. Can I collect them together?", answer: "No. The kits take up space and I will not hold them. Please collect within the stipulated timeline."},
-    {index: 6, question: "I want to preorder from you, can we arrange a meetup time when the item arrives?", answer: "This depends on how long it takes for the order to come. I cannot predict my availability, so meetups are usually arranged when the item arrives."},
+    {index: 6, question: "I want to preorder from you, can we arrange a meetup time when the item arrives?", answer: "I have a general schedule but cannot predict my availability. Usually I'd just arrange with buyers when the item arrives."},
     {index: 7, question: "Is the price negotiable?", answer: "Depends on the item, but generally no unless you are buying multiple items or are a regular customer."},
+    {index: 8, question: "The price on your website is cheaper than carousell, so is that the final price?", answer: "Only if you initiate a conversation with me through telegram and maintain it that way. Once the conversation hits carousell, it is whatever the price is on carousell."}
 ]
 
 export const FaqSection = () => {
